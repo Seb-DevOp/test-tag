ce3b6c5312738073b9615f15d524040844bea2c5
Seb GG <sebastien.giguet@ynov.com>
2025-04-25 10:00:15 +0200

    Feat-release.sh

--- release.sh
+++ release.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 TOOL=$1
+git config user.name "github-actions[bot]"
+git config user.email "github-actions[bot]@users.noreply.github.com"
 
 run_tests() {
   echo "✅ Lancement des tests..."
@@ -11,10 +13,21 @@ run_tests() {
   fi
 }
 
+get_commit_message() {
+  # Récupère le dernier message de commit
+  COMMIT_MESSAGE=$(git log -1 --pretty=%B)
+  echo "Dernier commit : $COMMIT_MESSAGE"
+}
+
 case $TOOL in
   standard-version)
     run_tests
     npx standard-version
+    get_commit_message # Appel de la fonction pour récupérer le commit
+    # Ajouter le commit message dans CHANGELOG
+    echo -e "\n\n### Dernier Commit: $COMMIT_MESSAGE" >> CHANGELOG.md
+    git add CHANGELOG.md
+    git commit -m "Update CHANGELOG.md with commit message"
     git push --follow-tags origin main
     gh release create $(node -p "require('./package.json').version") -F CHANGELOG.md
     ;;
