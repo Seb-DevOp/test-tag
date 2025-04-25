7b0691726881e134b372d266ce3938cd1c3cadc1
Seb GG <sebastien.giguet@ynov.com>
2025-04-25 10:09:02 +0200

    Fix changelog+Report

--- release.sh
+++ release.sh
@@ -5,10 +5,10 @@ git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
 
 run_tests() {
-  echo "✅ Lancement des tests..."
+  echo " Lancement des tests..."
   npm run test
   if [ $? -ne 0 ]; then
-    echo "❌ Tests échoués. Abandon de la release."
+    echo " Tests échoués. Abandon de la release."
     exit 1
   fi
 }
@@ -24,8 +24,8 @@ case $TOOL in
     run_tests
     npx standard-version
     get_commit_message # Appel de la fonction pour récupérer le commit
-    # Ajouter le commit message dans CHANGELOG
-    echo -e "\n\n### Dernier Commit: $COMMIT_MESSAGE" >> CHANGELOG.md
+    # Ajouter le commit message dans CHANGELOG, mais après la version générée par standard-version
+    echo -e "\n### Dernier Commit: $COMMIT_MESSAGE\n" >> CHANGELOG.md
     git add CHANGELOG.md
     git commit -m "Update CHANGELOG.md with commit message"
     git push --follow-tags origin main
